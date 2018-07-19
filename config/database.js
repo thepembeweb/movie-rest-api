59e538a11a236c099d414c9c9a2386ed26f332a8
Pemberai Sweto <pembeweb@yahoo.com>
2018-07-19 22:36:44 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 //Set up mongoose connection
 const mongoose = require('mongoose');
-const mongoDB = 'mongodb://localhost/node_rest_api';
+// const mongoDB = 'mongodb://localhost/node_rest_api';
+const mongoDB = 'mongodb://admin:admin01@ds143971.mlab.com:43971/movies';
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
